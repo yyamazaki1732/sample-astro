225e97fcddb34dcb0291a00d5185a00fcba3ca19
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-05-11 19:01:33 +0900

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import relativeLinks from "astro-relative-links";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // integrations: [relativeLinks()],
+  // outDir: './my-custom-build-directory'
+  site: 'https://www.my-site.dev'
+});
